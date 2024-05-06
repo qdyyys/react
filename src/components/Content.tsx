7b429c52601beb45daa2b82f6a78944ee2480cb1
qdyyys <qdyyys@gmail.com>
2024-05-06 03:48:20 +0300

    web

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,13 +2,19 @@ import weatherImage from '../../public/project_images/weather.png'
 import binanceImage from '../../public/project_images/binance.png'
 import panel from '../../public/project_images/panel.png'
 import devops from '../../public/project_images/devops.png'
-
 let slideImages = [binanceImage, weatherImage, panel, devops]
 import SwiperComp from "./SwiperComp"
+
+
 const Content = ()=>{
+
+    function Hello(){
+        alert('hello!')
+    }
     return (
-        <div className='h h-screen '>
-            <SwiperComp effect='cube' images={slideImages}></SwiperComp>
+        <div className='h h-screen flex flex-col items-center gap-10'>
+            <SwiperComp effect='cube' images={slideImages}></       SwiperComp>
+            <button onClick={Hello} className='text-white font-bold bg-lightRed-red py-2 px-6'>Нажать</button>
         </div>
     )
 }
